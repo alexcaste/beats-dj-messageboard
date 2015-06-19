7dbd48a0b3fc87e1a2754de7bda1c2934d5aefcf
alexcaste <alex.castaneda@gmail.com>
2015-06-19 09:17:47 -0700

    add initial files.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dj-messageboard',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
@@ -16,7 +18,17 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    contentSecurityPolicy: {
+     'default-src': "'none'",
+     'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
+     'font-src': "'self'",
+     'connect-src': "'self'",
+     'img-src': "'self'",
+     'style-src': "'self' 'unsafe-inline'",
+     'frame-src': "'none'"
+   }
   };
 
   if (environment === 'development') {
